45d67ae41cced8cbce7a7abce23e46a5a8d5c6f2
yosef <yosefsusanto7@gmail.com>
2023-06-27 23:53:46 +0700

    kedua

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 """
-print("hello")
+Aplikasi Deteksi Gempa Terkini
 """
 
 print("hello")
+
+"""
+Waktu : 27 Juni 2023, 20:32:59 WIB
+Magnitudo : 4.3
+Kedalaman : 14 km
+Lokasi 6.95 LS - 105.40 BT
+Pusat gempa berada di laut 37 km BaratDaya Sumur
+Dirasakan (Skala MMI): II-III Sumur, II Munjul
+"""
