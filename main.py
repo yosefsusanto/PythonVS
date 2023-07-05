861fb29fe58b12cb129a118f0c8657eacf3dae01
yosef <yosefsusanto7@gmail.com>
2023-07-05 18:42:47 +0700

    pembaharuan data bmkg

--- main.py
+++ main.py
@@ -2,13 +2,44 @@
 Aplikasi Deteksi Gempa Terkini
 """
 
-print("hello")
+def ekstrasi_data():
+    """
+    Tanggal : 05 Juli 2023
+    Waktu : 14:31:29 WIB
+    Magnitudo : 4.5
+    Kedalaman : 3 km
+    Lokasi : LS=2.41 - BT=120.88
+    Keterangan : Pusat gempa berada di laut 37 km BaratDaya Sumur
+    Dirasakan : Dirasakan (Skala MMI): II-III Sumur, II Munjul
+    :retrun:
+    """
+    hasil = dict()
+    hasil['tanggal'] = '05 Juli 2023'
+    hasil['waktu'] =  '14:31:29 WIB'
+    hasil['magnitudo'] = 4.5
+    hasil['kedalaman'] = '3 km'
+    hasil['lokasi'] = {'LS' = 2.41, 'BT' = 120.88}
+    hasil['keterangan'] = 'Pusat gempa berada di laut 37 km BaratDaya Sumur'
+    hasil['dirasakan'] = 'Dirasakan (Skala MMI): II-III Sumur, II Munjul'
+    return hasil
 
-"""
-Waktu : 27 Juni 2023, 20:32:59 WIB
-Magnitudo : 4.3
-Kedalaman : 14 km
-Lokasi 6.95 LS - 105.40 BT
-Pusat gempa berada di laut 37 km BaratDaya Sumur
-Dirasakan (Skala MMI): II-III Sumur, II Munjul
-"""
+def tampilkan_data(result):
+    print(f"Gempa terakhir berdasarkan BMKG")
+    print(f"Tanggal {result['tanggal']}")
+    print(f"Waktu {result['waktu']}")
+    print(f"Magnitudo {result['magnitudo']}")
+    print(f"Kedalaman {result['kedalaman']}")
+    print(f"Lokasi {result['lokasi']}")
+    print(f"Keterangan {result['keterangan']}")
+    print(f"dirasakan {result['dirasakan']}")
+
+    
+    
+
+if __name__ == '__main__':
+    print("Aplikasi utama")
+    result = ekstrasi_data()
+    tampilkan_data(result)
+
+
+
